c2e67631aa5e614cc8662bd8731eab8d496d16fd
Kiselev Egor <kiselev.el@phystech.edu>
2022-02-09 08:16:50 +0300

    Some fixes

--- list/list.h
+++ list/list.h
@@ -11,70 +11,84 @@ namespace lab618
         {
             T data;
             leaf * pnext;
-            leaf(T& _data, leaf * _pnext)
-            {
-            }
+            leaf(T& _data, leaf * _pnext) :
+                data(_data),
+                pnext(_pnext)
+            {}
         };
     public:
         class CIterator
         {
         public:
-            CIterator()
-                : m_pCurrent(0), m_pBegin(0)
-            {
-            }
+            CIterator() :
+                m_pCurrent(nullptr),
+                m_pBegin(nullptr)
+            {}
 
-            CIterator(leaf *p)
-            {
-            }
+            CIterator(leaf *p) :
+                m_pCurrent(p),
+                m_pBegin(nullptr)
+            {}
 
-            CIterator(const CIterator &src)
-            {
-            }
+            CIterator(const CIterator &src) :
+                m_pCurrent(src.m_pCurrent),
+                m_pBegin(src.m_pBegin)
+            {}
 
             ~CIterator()
             {
+                m_pCurrent = nullptr;
+                m_pBegin = nullptr;
             }
 
             CIterator& operator = (const CIterator&  src)
             {
+                m_pCurrent = src.m_pCurrent;
+                m_pBegin = src.m_pBegin;
+                return *this;
             }
 
             bool operator != (const CIterator&  it) const
             {
-                return true;
+                return m_pCurrent != it.m_pCurrent;
             }
 
             void operator++()
             {
+                if (m_pCurrent != nullptr)
+                    m_pCurrent = m_pCurrent->pnext;
+                else
+                    m_pCurrent = m_pBegin;
             }
 
             T& getData()
             {
-                return T();
+                return m_pCurrent->data;
             }
 
             T& operator* ()
             {
-                T tmp;
-                return tmp;
+                return m_pCurrent->data;
             }
 
             leaf* getLeaf()
             {
-                return 0;
+                return m_pCurrent;
             }
 
             void setLeaf(leaf* p)
             {
+                m_pCurrent = p;
             }
 
             void setLeafPreBegin(leaf* p)
             {
+                m_pBegin = p;
             }
 
-            bool isValid() {
-                return false;
+            bool isValid()
+            {
+                return m_pCurrent != nullptr;
             }
 
         private:
@@ -86,45 +100,106 @@ namespace lab618
 
     public:
 
-        CSingleLinkedList()
-        {
-        }
+        CSingleLinkedList() :
+            m_pBegin(nullptr),
+            m_pEnd(nullptr)
+        {}
 
         virtual ~CSingleLinkedList()
         {
+            clear();
         }
 
         void pushBack(T& data)
         {
+            if (m_pEnd != nullptr)
+            {
+                m_pEnd->pnext = new leaf(data, nullptr);
+                m_pEnd = m_pEnd->pnext;
+            }
+            else
+            {
+                m_pEnd = new leaf(data, nullptr);
+                m_pBegin = m_pEnd;
+            }
         }
 
         void pushFront(T& data)
         {
+            if (m_pBegin != nullptr)
+                m_pBegin = new leaf(data, m_pBegin);
+            else
+            {
+                m_pBegin = new leaf(data, nullptr);
+                m_pEnd = m_pBegin;
+            }
         }
 
         T popFront()
         {
-            T tmp;
-            return tmp;
+            if (m_pBegin == nullptr)
+                throw "List is empty!";
+
+            leaf *tmp_ptr = m_pBegin;
+            T tmp_data = m_pBegin->data;
+            m_pBegin = m_pBegin->pnext;
+            delete tmp_ptr;
+
+            return tmp_data;
         }
 
         // изменяет состояние итератора. выставляет предыдущую позицию.
         void erase(CIterator& it)
         {
+            leaf *pcurr = it.getLeaf();
+            leaf *pnext = pcurr->pnext;
+            leaf *pprev = m_pBegin;
+
+            while (pprev->pnext != pcurr) {
+                pprev = pprev->pnext;
+            }
+
+            if (pprev != nullptr) {
+                pprev->pnext = pnext;
+                it.setLeaf(pprev);
+            }
+            else {
+                it.setLeaf(nullptr);
+                it.setLeafPreBegin(m_pBegin);
+            }
+
+            delete pcurr;
         }
 
         int getSize()
         {
-            return 0;
+            int size = 0;
+            leaf *pcurr = m_pBegin;
+
+            while (pcurr != nullptr)
+            {
+                ++size;
+                pcurr = pcurr->pnext;
+            }
+
+            return size;
         }
 
         void clear()
         {
+            while (m_pBegin != nullptr)
+            {
+                leaf *pcurr = m_pBegin;
+                m_pBegin = m_pBegin->pnext;
+                delete pcurr;
+            }
+
+            m_pEnd = nullptr;
         }
 
         CIterator begin()
         {
-            return CIterator();
+            return CIterator(m_pBegin);
         }
 
     private:
@@ -139,80 +214,106 @@ namespace lab618
         {
             T data;
             leaf * pnext, *pprev;
-            leaf(T& _data, leaf * _pprev, leaf * _pnext)
-            {
-            }
+            leaf(T& _data, leaf * _pprev, leaf * _pnext) :
+                data(_data),
+                pprev(_pprev),
+                pnext(_pnext)
+            {}
         };
     public:
         class CIterator
         {
         public:
-            CIterator()
-            {
-            }
+            CIterator() :
+                m_pBegin(nullptr),
+                m_pCurrent(nullptr),
+                m_pEnd(nullptr)
+            {}
 
-            CIterator(leaf *p)
-            {
-            }
+            CIterator(leaf *p) :
+                m_pBegin(nullptr),
+                m_pCurrent(p),
+                m_pEnd(nullptr)
+            {}
 
-            CIterator(const CIterator &src)
-            {
-            }
+            CIterator(const CIterator &src) :
+                m_pBegin(src.m_pBegin),
+                m_pCurrent(src.m_pCurrent),
+                m_pEnd(src.m_pEnd)
+            {}
 
             ~CIterator()
             {
+                m_pBegin = nullptr;
+                m_pCurrent = nullptr;
+                m_pEnd = nullptr;
             }
 
             CIterator& operator = (const CIterator&  src)
             {
+                m_pBegin = src.m_pBegin;
+                m_pCurrent = src.m_pCurrent;
+                m_pEnd = src.m_pEnd;
+
+                return *this;
             }
 
             bool operator != (const CIterator&  it) const
             {
+                return m_pCurrent != it.m_pCurrent;
             }
 
             void operator++()
             {
+                if (m_pCurrent != nullptr)
+                    m_pCurrent = m_pCurrent->pnext;
+                else
+                    m_pCurrent = m_pBegin;
             }
 
             void operator--()
             {
+                if (m_pCurrent != nullptr)
+                    m_pCurrent = m_pCurrent->pprev;
+                else
+                    m_pCurrent = m_pEnd;
             }
 
             T& getData()
             {
-                T tmp;
-                return tmp;
+                return m_pCurrent->data;
             }
 
             T& operator* ()
             {
-                T tmp;
-                return tmp;
+                return m_pCurrent->data;
             }
 
             leaf* getLeaf()
             {
-                return 0;
+                return m_pCurrent;
             }
 
             // применяется в erase и eraseAndNext
             void setLeaf(leaf* p)
             {
+                m_pCurrent = p;
             }
 
             // применяется в erase и eraseAndNext
             void setLeafPreBegin(leaf* p)
             {
+                m_pBegin = p;
             }
 
             // применяется в erase и eraseAndNext
             void setLeafPostEnd(leaf* p)
             {
+                m_pEnd = p;
             }
 
             bool isValid() {
-                return false;
+                return m_pCurrent != nullptr;
             }
 
         private:
@@ -226,61 +327,150 @@ namespace lab618
 
     public:
 
-        CDualLinkedList()
-        {
-        };
+        CDualLinkedList() :
+            m_pBegin(nullptr),
+            m_pEnd(nullptr)
+        {}
 
         virtual ~CDualLinkedList()
         {
-        };
+            clear();
+        }
 
         void pushBack(T& data)
         {
+            if (m_pEnd != nullptr)
+            {
+                m_pEnd->pnext = new leaf(data, m_pEnd, nullptr);
+                m_pEnd = m_pEnd->pnext;
+            }
+            else
+                m_pEnd = new leaf(data, nullptr, nullptr);
+                m_pBegin = m_pEnd;
         }
 
         T popBack()
         {
-            T tmp;
-            return tmp;
+            if (m_pEnd == nullptr)
+                throw "List is empty!";
+
+            leaf *tmp_ptr = m_pEnd;
+            T tmp_data = m_pEnd->data;
+            m_pEnd = m_pEnd->pprev;
+            m_pEnd->pnext = nullptr;
+            delete tmp_ptr;
+
+            return tmp_data;
         }
 
         void pushFront(T& data)
         {
+            if (m_pBegin != nullptr)
+            {
+                m_pBegin->pprev = new leaf(data, nullptr, m_pBegin);
+                m_pBegin = m_pBegin->pprev;
+            }
+            else
+            {
+                m_pBegin = new leaf(data, nullptr, nullptr);
+                m_pEnd = m_pBegin;
+            }
         }
 
         T popFront()
         {
-            T tmp;
-            return tmp;
+            if (m_pBegin == nullptr)
+                throw "List is empty!";
+
+            leaf *tmp_ptr = m_pBegin;
+            T tmp_data = m_pBegin->data;
+            m_pBegin = m_pBegin->pnext;
+            m_pBegin->pprev = nullptr;
+            delete tmp_ptr;
+
+            return tmp_data;
         }
 
         // изменяет состояние итератора. выставляет предыдущую позицию.
         void erase(CIterator& it)
         {
+            leaf *pcurr = it.getLeaf();
+            leaf *pnext = pcurr->pnext;
+            leaf *pprev = pcurr->pprev;
+
+            if (pnext != nullptr) {
+                pnext->pprev = pprev;
+            }
+
+            if (pprev != nullptr) {
+                pprev->pnext = pnext;
+                it.setLeaf(pprev);
+            }
+            else {
+                it.setLeaf(nullptr);
+                it.setLeafPreBegin(m_pBegin);
+            }
+
+            delete pcurr;
         }
 
         // изменяет состояние итератора. выставляет следующую позицию.
         void eraseAndNext(CIterator& it)
         {
+            leaf *pcurr = it.getLeaf();
+            leaf *pnext = pcurr->pnext;
+            leaf *pprev = pcurr->pprev;
+
+            if (pprev != nullptr) {
+                pprev->pnext = pnext;
+            }
+
+            if (pnext != nullptr) {
+                pnext->pprev = pprev;
+                it.setLeaf(pnext);
+            }
+            else {
+                it.setLeaf(nullptr);
+                it.setLeafPostEnd(m_pEnd);
+            }
+
+            delete pcurr;
         }
 
         int getSize()
         {
-            return 0;
+            int size = 0;
+            leaf *pcurr = m_pBegin;
+
+            while (pcurr != nullptr)
+            {
+                ++size;
+                pcurr = pcurr->pnext;
+            }
+
+            return size;
         }
 
         void clear()
         {
+            while (m_pBegin != nullptr)
+            {
+                leaf *pcurr = m_pBegin;
+                m_pBegin = m_pBegin->pnext;
+                delete pcurr;
+            }
+
+            m_pEnd = nullptr;
         }
 
         CIterator begin()
         {
-            return CIterator();
+            return CIterator(m_pBegin);
         }
 
         CIterator end()
         {
-            return CIterator();
+            return CIterator(m_pEnd);
         }
 
     private:
--- list/main.cpp
+++ list/main.cpp
@@ -8,5 +8,25 @@ using lab618::CSingleLinkedList;
 int main() {
 	CSingleLinkedList<int> sl_list;
 
+	int a = 10;
+
+	printf("Size: %d\n", sl_list.getSize());
+	sl_list.pushBack(a);
+	printf("Size: %d\n", sl_list.getSize());
+	sl_list.pushBack(a);
+	printf("Size: %d\n", sl_list.getSize());
+	sl_list.pushBack(a);
+	printf("Size: %d\n", sl_list.getSize());
+	//a += 10;
+	//sl_list.pushBack(a);
+	//a += 10;
+	//sl_list.pushBack(a);
+
+	sl_list.popFront();
+	printf("Size: %d\n", sl_list.getSize());
+	sl_list.popFront();
+	printf("Size: %d\n", sl_list.getSize());
+	sl_list.popFront();
+	printf("Size: %d\n", sl_list.getSize());
 	return 0;
 }
