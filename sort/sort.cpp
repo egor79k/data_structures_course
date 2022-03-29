cff1944b2fd98e06d4481b354acd3da6982ef536
egor <kiselev.el@phystech.edu>
2022-03-29 12:18:36 +0300

    Sort.cpp added

---
+++ sort/sort.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+
+#include "sort.h"
+
+
+namespace templates
+{
+    void mergeSortRecursive(void **ppArray, void **ppTemp, int length, CompareSortType pCompareFunc)
+    {
+        if (length < 2)
+        {
+            return;
+        }
+
+        // Рекурсивно делим на подзадачи
+        const int middle = length / 2;
+        mergeSortRecursive(ppArray, ppTemp, middle, pCompareFunc);
+        mergeSortRecursive(ppArray + middle, ppTemp, length - middle, pCompareFunc);
+
+        // Объединяем два отсортированных подмассива
+        int left_id = 0;
+        int right_id = middle;
+
+        for (int i = 0; i < length; ++i)
+        {
+            if ((left_id < middle) && ((right_id >= length) || pCompareFunc(ppArray[left_id], ppArray[right_id]) > 0))
+            {
+                // Если текущий элемент левого массива меньше (или во втором
+                // массиве не осталось элементов), то следующим кладем его
+                ppTemp[i] = ppArray[left_id];
+                ++left_id;
+            }
+            else
+            {
+                // Иначе кладем текущий элемент из правого массива
+                ppTemp[i] = ppArray[right_id];
+                ++right_id;   
+            }
+        }
+
+        // Копируем объединенный массив из временной памяти
+        for (int i = 0; i < length; ++i)
+        {
+            ppArray[i] = ppTemp[i];
+        }
+    }
+
+
+    void mergeSort(void **ppArray, int length, CompareSortType pCompareFunc)
+    {
+        void **ppTemp = new void*[length];
+
+        mergeSortRecursive(ppArray, ppTemp, length, pCompareFunc);
+
+        delete[] ppTemp;
+    }
+}; // namespace templates
